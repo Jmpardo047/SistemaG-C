b8fbbd7bc63733b39f59ed1d5fa8831691057819
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2024-02-28 12:53:59 -0500

    m1

---
+++ modules/persona.py
@@ -0,0 +1,129 @@
+import os
+import modules.menus as m
+import modules.corefiles as c
+def menpersonas(personas : dict):
+    v = m.MenuControl()
+    if (v == 1):
+        Addpersona(personas)
+    elif (v == 2):
+        pass
+    elif (v == 3):
+        pass
+    elif (v == 4):
+        pass
+    elif (v == 5):
+        pass
+
+def Addpersona(personas : dict):
+    listtipos = ['movil','casa','personal','oficina']
+    print('Bienvenido al sistema de añadir personas')
+    print('Escriba el tipo de persona que es (natural (N) o juridica (J))')
+    tipo = c.ValidStr().upper()
+    if (tipo == 'N'):
+        print('Ingrese el numuero de cedula de la persona')
+        cc = c.validInt()
+        print(f'Ingrese el nombre de {cc}')
+        name = c.ValidStr()
+        print(f'Ingrese el email de {name}')
+        email = c.ValidStr()
+        persona = {
+            'cc' : cc,
+            'name' :  name,
+            'email': email,
+            'telefono':{
+                'movil': [],
+                'casa' : [],
+                'personal' : [],
+                'oficina' : []
+            }
+        }
+        print(f'Desea agregar un telefono al registro de {name}? S(si) N(no)')
+        add = c.ValidStr().upper()
+        if (add == 'S'):
+            isactive = True
+            while isactive:
+                print('Que tipo telefono quiere agregar')
+                print(listtipos)
+                tipo = c.ValidStr().lower()
+                if (tipo in listtipos):
+                    print('Escriba el numuero de telefono que desea agregar')
+                    tel = c.validInt()
+                    persona['telefono'][tipo].append(tel)
+                    print('Desea agregar otro telefono? Si (S) No (N)')
+                    rete = c.ValidStr().upper()
+                    if (rete == 'S'):
+                        isactive = True
+                    elif(rete == 'N'):
+                        isactive = False
+                        personas['personasn'].update({cc:persona})
+                    else:
+                        isactive = False
+                        print('Opcion escrita no es correcta, no se agregó ninugn registro al sistema de personas')
+                        os.system('pause')
+                        return
+                else:
+                    print('No se pudo encontrar el tipo de telefono, vuelvalo a intentar')
+                    isactive = True            
+        elif (add == 'N'):
+            personas['personasn'].update({cc:persona})
+            print(f'La persona {name} ha sido agregada correctamente')
+            os.system('pause')
+            return
+        else:
+            print('opcion ingresada no valida')
+            os.system('pause')
+            Addpersona(personas)
+    elif (tipo == 'J'):
+        print('Ingrese el numuero NIT del usuario')
+        nit = c.validInt()
+        print(f'Ingrese el nombre del usuario {nit}')
+        name = c.ValidStr
+        print(f'Ingrese el email de {name}')
+        email = c.validInt()
+        persona = {
+            'cc' : cc,
+            'name' :  name,
+            'email': email,
+            'telefono':{
+                'movil': [],
+                'casa' : [],
+                'personal' : [],
+                'oficina' : []
+            }
+        }
+        print(f'Desea agregar un telefono al registro de {name}? S(si) N(no)')
+        add = c.ValidStr().upper()
+        if (add == 'S'):
+            isactive = True
+            while isactive:
+                print('Que tipo telefono quiere agregar')
+                print(listtipos)
+                tipo = c.ValidStr().lower()
+                if (tipo in listtipos):
+                    print('Escriba el numuero de telefono que desea agregar')
+                    tel = c.validInt()
+                    persona['telefono'][tipo].append(tel)
+                    print('Desea agregar otro telefono? Si (S) No (N)')
+                    rete = c.ValidStr().upper()
+                    if (rete == 'S'):
+                        isactive = True
+                    elif(rete == 'N'):
+                        isactive = False
+                        personas['personasj'].update({cc:persona})
+                    else:
+                        isactive = False
+                        print('Opcion escrita no es correcta, no se agregó ninugn registro al sistema de personas')
+                        os.system('pause')
+                        return
+                else:
+                    print('No se pudo encontrar el tipo de telefono, vuelvalo a intentar')
+                    isactive = True            
+        elif (add == 'N'):
+            personas['personasj'].update({cc:persona})
+            print(f'La persona {name} ha sido agregada correctamente')
+            os.system('pause')
+            return
+    else:
+        print('El tipo de persona seleccionado no es valido')
+        os.system('pause')
+        Addpersona(personas)
